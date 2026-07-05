832b9f2140070c05635cf9fb96b58914e456c2dd
aidaelizabathvarghese2003@gmail.com <aidaelizabathvarghese2003@gmail.com>
2026-07-05 21:35:10 +0530

    ...

--- Program2.java
+++ Program2.java
@@ -3,6 +3,8 @@ class Program2
 	public static void main(String[] args) 
 	{
 		smallest ();
+		oddNum();
+		evenNum();
 	}
 	
 	public static void smallest(){
@@ -21,4 +23,41 @@ class Program2
 		    System.out.println(" the smallest digit in " + original  + " is " + small);
 			
 	}
+	public static void oddNum(){
+		int num = 123467;
+		int count =0;
+		int temp = num;
+		while (num!=0)
+		{
+			int rev = num%10;
+			if (rev%2!=0)
+		{
+			count++;
+		}
+		   num/=10;
+		}
+		
+		System.out.println("The count of odd digits in " +temp+ " is " + count);
+		
+	}
+	
+	public static void evenNum(){
+		int num = 45723;
+		int count =0;
+		int temp = num;
+		while (num!=0)
+		{
+			int rev = num%10;
+			if (rev%2==0)
+		{
+			count++;
+		}
+		   num/=10;
+		}
+		
+		System.out.println("The count of even digits in " +temp+ " is " + count);
+		
+	}
+	
+	
 	}
