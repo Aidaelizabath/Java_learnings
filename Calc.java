9c8400498e166d00e1a79dc4b7b481914bf3c147
aidaelizabathvarghese2003@gmail.com <aidaelizabathvarghese2003@gmail.com>
2026-07-06 20:41:31 +0530

    ...

---
+++ Calc.java
@@ -0,0 +1,29 @@
+class Calc
+{
+	public static void main(String[] args) 
+	{  
+	   
+	   int a =10;
+	   int b = 5;
+	   char op ='-';
+		switch(op){
+			case '+':
+				System.out.println(a+b);
+				break;
+			
+			case '-':
+				System.out.println(a-b);
+				break;
+			
+			case '*':
+				System.out.println(a*b);
+				break;
+			
+			case '/':
+				System.out.println(a/b);
+				break;
+			
+		}
+		
+	}
+}
