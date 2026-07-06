bd027d666d2ac46420168b817cc47b5c26b0a85c
aidaelizabathvarghese2003@gmail.com <aidaelizabathvarghese2003@gmail.com>
2026-07-06 20:37:52 +0530

    ...

---
+++ Arithmetic.java
@@ -0,0 +1,26 @@
+class Arithmetic 
+{
+	public static void main(String[] args) 
+	{
+		int a = 5;
+		int b = 2;
+		System.out.println(a+b);
+		int c= a+b;
+		System.out.println(c);
+		int d = 2;
+		double e = 2.5d;
+		System.out.println(d+e);
+		int f = (int)(d+e);
+		System.out.println(f);	
+		char g ='A';
+		int h= 2;
+		System.out.println(g+h);
+		char j =(char) (g+h);
+		System.out.println(j);
+		System.out.println(10+20 +" hai");
+		System.out.println(30 +" hai "+40+60);
+		System.out.println("Java " +'C');
+		
+	}
+}
+
