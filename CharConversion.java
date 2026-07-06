f2f6451817e885ca62f5aaabecd1df8c23941945
aidaelizabathvarghese2003@gmail.com <aidaelizabathvarghese2003@gmail.com>
2026-07-06 20:42:53 +0530

    ...

---
+++ CharConversion.java
@@ -0,0 +1,32 @@
+class CharConversion 
+{
+	public static void main(String[] args)
+	{
+		double d1 = 97d;
+		char c1 = (char) d1;
+		System.out.println(d1);
+		System.out.println(c1);
+		
+		float f2 = 98f;
+		char c2= (char) f2;
+		System.out.println(f2);
+		System.out.println(c2);
+		
+		long l3 = 99;
+		char c3 = (char) l3;
+		System.out.println(l3);
+		System.out.println(c3);
+		
+		int i4 =100;
+		char c4 = (char) i4;
+		System.out.println(i4);
+		System.out.println(c4);
+		
+		short s5 = 101;
+		char c5 = (char) s5;
+		System.out.println(s5);
+		System.out.println(c5);
+		
+		
+	}
+}
