f1c235e4376bdb0d6bc38613a3284a2ba5e64be5
aidaelizabathvarghese2003@gmail.com <aidaelizabathvarghese2003@gmail.com>
2026-07-06 20:44:14 +0530

    ...

---
+++ D1.java
@@ -0,0 +1,19 @@
+class  D1
+{
+	public static void main(String[] args) 
+	{
+		System.out.println(m1(10,20));
+		System.out.println(m2());
+		
+	}
+	public static int m1(int a ,int b){
+		System.out.println(a+b);
+		return 10;
+	}
+	public static String m2(){
+		return "A";
+	}
+	}
+		
+		
+
